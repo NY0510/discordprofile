ce7931910ee46c68c8e9da2a8910d375cc6bbc6f
namnyang <74480667+namnyang@users.noreply.github.com>
2021-01-15 15:46:05 +0900

    .

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// url 에서 parameter 추출하는 함수
+function getParam(sname) {
+    var params = location.search.substr(location.search.indexOf("?") + 1);
+    var sval = "";
+    params = params.split("&");
+    for (var i = 0; i < params.length; i++) {
+        temp = params[i].split("=");
+        if ([temp[0]] == sname) {
+            sval = temp[1];
+        }
+    }
+    console.log(sval);
+    return sval;
+}
+function getAvatare() {
+    id = document.getElementById("usernameInput").value;
+    var img = document.getElementById("Profilephoto");
+    imgurl = "https://d.lu.je/avatar/" + id + "?size=256";
+    img.onload = function () {
+        document.getElementById("downloadimg").style.display = "block";
+    };
+    img.src = imgurl;
+    // window.open("https://d.lu.je/avatar/" + id + "/?size=2048", "_blank");
+}
+function downloadimg() {
+    window.open(imgurl, "_blank");
+}
